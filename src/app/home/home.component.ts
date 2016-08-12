44ee81559db88af484a80304275b00f7f3171a03
Jaytee Sanchez <jsanchez@nationalmerchants.com>
2016-08-12 07:15:19 -0700

    rename app component to home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../../../public/css/styles.css';
+@Component({
+  selector: 'my-app',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent { }
