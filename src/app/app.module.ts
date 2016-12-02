3c15c05821f29ed0c732b43ca628a3fc2b82d7e0
Jaytee Sanchez <jsanchez@nationalmerchants.com>
2016-12-02 10:51:01 -0800

    change app component to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    HomeComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ HomeComponent ]
 })
 export class AppModule { }
